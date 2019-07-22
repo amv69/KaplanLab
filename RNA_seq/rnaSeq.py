e9b05c0bef8f94c3eb32a81ead9c22065d485f76
Alex Visbisky <amv69+@pitt.edu>
2019-07-22 11:49:32 -0400

    rnaSeq.py update

--- RNA_seq/rnaSeq.py
+++ RNA_seq/rnaSeq.py
@@ -78,39 +78,26 @@ subprocess.check_call("for file in *sorted.bam; do echo $file && samtools idxsta
 
 subprocess.check_call("for file in *_unique_sorted_mt.bam; do echo $file && bedtools bamtobed -cigar -i $file > ${file/_unique_sorted_mt.bam/.bed}; done", shell=True)
 
-"""
-BIG DISCLAIMER: TO-DO
-
-I wrote this to merge bed files here, it should be down prior to bigwig conversion (probably)
-
-"""
-
-#Sorts the bedgraph files to ensure bedGraphToBigWig works correctly
-subprocess.check_call("for file in *.bed; do echo $file && LC_COLLATE=C sort -k1,1 -k2,2n $file > ${file/.bed/_sorted.bed}; done", shell=True)
-
-#Here it is ^
-subprocess.check_call("for file in *.bed; do echo $file && bedtools merge -i $file -s -c 6 -o distinct  > ${file/.bed/_merged.bed}; done", shell=True)
-
 #Get genomecoverage for bed files
-subprocess.call('for str in "+" "-"; do [ "$str" = "+" ] && n="rev" || n="fw"; for file in *_merged.bed; do sample=${file} && echo $n $sample && bedtools genomecov -g /bgfs/ckaplan/Anand_seq/Genomes/Combined_yeast/Budding_Fission_new.genome -i $file -bg -5 -strand $str > ${sample}_${n}.bg; done; done', shell=True)
+subprocess.check_call('for str in "+" "-"; do [ "$str" = "+" ] && n="rev" || n="fw"; for file in *.bed; do sample=${file} && echo $n $sample && bedtools genomecov -g /bgfs/ckaplan/Anand_seq/Genomes/Combined_yeast/Budding_Fission_new.genome -i $file -bg -5 -strand $str > ${sample}_${n}.bg; done; done', shell=True)
 
 #Function to generate full BedGraph files that we want, There are Triple Strings to deliminate file 
 subprocess.check_call("""f_str="fw"; r_str="rev"; ext=".bg"; for file1 in *${f_str}${ext}; do file2=${file1/${f_str}/${r_str}} && outfile=${file1/${f_str}${ext}/fw_rev.bedgraph} && echo $file1 "+" $file2 "=" $outfile && awk 'BEGIN{OFS="\t"}{print $1,$2,$3,"-"$4}' $file2 | cat $file1 - | sort -k1,1 -k2,2n > $outfile; done""", shell=True)
 
 #awk command to only get chromosomes that appear on the genome file
-subprocess.check_call("for file in *.bedgraph; do echo $file && awk '$1 ~ /chr/' $file > ${file/.bedgraph/.bedgraph}; done", shell=True)
+subprocess.check_call("for file in *.bedgraph; do echo $file && awk '$1 ~ /chr/' $file > ${file/.bedgraph/_chr.bedgraph}; done", shell=True)
 
 #Sorts the bedgraph files to ensure bedGraphToBigWig works correctly
-subprocess.check_call("for file in *.bedgraph; do echo $file && LC_COLLATE=C sort -k1,1 -k2,2n $file > ${file/.bedgraph/_sorted.bedgraph}; done", shell=True)
+subprocess.check_call("for file in *chr.bedgraph; do echo $file && LC_COLLATE=C sort -k1,1 -k2,2n -k3,3n -s  $file > ${file/.bedgraph/_sorted.bedgraph}; done", shell=True)
+
+#merge files so they can be converted to bigwig (There can't be overlap)
+subprocess.check_call("for file in *sorted.bedgraph; do echo $file && bedtools merge -i $file -c 4 -d 0 -o max > ${file/sorted.bedgraph/merged.bedgraph}; done", shell=True)
+
+#Sorts the bedgraph files to ensure bedGraphToBigWig works correctly
+subprocess.check_call("for file in *merged.bedgraph; do echo $file && LC_COLLATE=C sort -k1,1 -k2,2n -k3,3n -s  $file > ${file/.bedgraph/_sorted2.bedgraph}; done", shell=True)
 
 #Conversion to BigWIg
-subprocess.check_call("for file in *.bedgraph; do echo $file && bedGraphToBigWig $file /bgfs/ckaplan/Anand_seq/Genomes/Combined_yeast/Budding_Fission_new.genome $file.bw", shell=True)
-
-
-
-
-
-
+subprocess.check_call("for file in *sorted2.bedgraph; do echo $file && bedGraphToBigWig $file /bgfs/ckaplan/Anand_seq/Genomes/Combined_yeast/Budding_Fission_new.genome $file.bw; done", shell=True)
 
 
 #Returns to the original working directory
