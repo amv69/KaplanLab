3e3017ee1dd66c286e4545ba2cf9172058f5fef7
Alex Visbisky <amv69+@pitt.edu>
2019-10-14 17:52:24 -0400

    Updated Scripts

--- compare3.py
+++ compare3.py
@@ -9,22 +9,16 @@ fasta = sys.argv[2]
 
 print(fname)
 outFile = fname[:-9] + "_match.txt"
-missFile = fname[:-9] + "_miss.txt"
-noneFile = fname[:-9] + "_none.txt"
 out = open(noneFile, "w+")
-with gzip.open(fname) as f:
-    lines = f.readlines()
-    reads = lines[1::4]
-    with open(fasta) as f2:
-        fastaLines = f2.readlines()
-        fastaReads = lines[1::2]
-        for read in reads:
-            if read in fastaReads:
+with open(fasta) as f2:
+    fastaLines = f2.readlines()
+    fastaReads = fastaLines[1::2]
+    for read in fastaReads:
+        read = read[26:-27]
+        print("Searching for: " + str(read) + "\nin: " + str(fasta))
+        command = "gunzip -c " + fname + " | grep -i -c " + str(read).strip('\'').strip('b\'').strip('\\n') + " >> " + outFile
+        subprocess.call(command,shell=True) 
+            #if read.lower() in map(str.lower, fastaReads):
                 #subprocess.call(gsed -n '2~4p' , shell=True)
-                cmd = "grep -i -B 1 " + str(read).strip('\'').strip('b\'').strip('\\n') + " " + fasta + " | head -1 >> " + outFile
-                subprocess.call(cmd, shell=True)
-            elif read not in fastaReads:
-                out.write(read)
-        subprocess.call("uniq -u " + outFile + "> uniq_" + outFile, shell=True)
-        subprocess.call("sed '2~2d' " + fasta + "> names_" + fasta, shell=True)
-        subprocess.call("diff names_" + fasta + "uniq_" + outFile + "> " + missFile)
+               # cmd = "grep -i -B 1 " + str(read).strip('\'').strip('b\'').strip('\\n') + " " + fasta + " | head -1 >> " + outFile
+                #subprocess.call(cmd, shell=True)
